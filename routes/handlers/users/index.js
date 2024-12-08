9337441bf2f2f61c682d4932d9d1e947dcc047df
elazsyahranie <elazaribrahims@gmail.com>
2024-12-08 20:00:57 +0700

    delete

--- routes/handlers/users/index.js
+++ routes/handlers/users/index.js
@@ -1,9 +1,11 @@
 const register = require('./register');
 const login = require('./login');
 const update = require('./update');
+const deleteUser = require('./delete');
 
 module.exports = {
   register,
   login,
   update,
+  deleteUser,
 };
